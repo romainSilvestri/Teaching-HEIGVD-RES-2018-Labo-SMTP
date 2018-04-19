27782d61f2e3ce927dc583c3829948e8154fa4f1
romain_silvestri <romain.silvestri@heig-vd.ch>
2018-04-19 11:16:52 +0200

    changing protocol

--- src/main/java/smtp/Protocol.java
+++ src/main/java/smtp/Protocol.java
@@ -1,4 +1,11 @@
 package smtp;
 
-public class protocol {
+public class Protocol {
+
+    public final static String CMD_END_DATA = "\r\n.\r\n";
+    public final static String CMD_MAIL_FROM = "MAIL FROM: ";
+    public final static String CMD_QUIT = "quit\r\n";
+    public final static String CMD_RCPT_TO = "RCPT TO: ";
+    public final static String CMD_DATA = "DATA";
+    public final static String CMD_HELLO = "EHLO ";
 }
